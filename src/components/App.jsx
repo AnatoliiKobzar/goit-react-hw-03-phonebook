f71b582c8645d3263ce9673ffb24470891c7cf44
KobzarAnatolii <TohaPirat@gmail.com>
2023-02-27 16:36:27 +0200

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,11 @@ import { Filter } from './Filter/Filter';
 import { GlobalStyle } from './GlobalStyle/GlobalStyle';
 import { Layout } from './Layout/Layout';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import { getFilteredArray } from 'utils/getFilteredArray';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -63,19 +59,25 @@ class App extends Component {
     this.setState({ filter: event.currentTarget.value });
   };
 
-  getFiltredContacts = () => {
-    const { contacts, filter } = this.state;
+  componentDidMount() {
+    const parseContact = JSON.parse(localStorage.getItem('contacts'));
 
-    const normalizedContacts = filter.toLowerCase();
+    if (parseContact) {
+      this.setState({ contacts: parseContact });
+    }
+  }
 
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedContacts)
-    );
-  };
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFiltredContacts();
+    const { filter, contacts } = this.state;
+    const filteredContacts = getFilteredArray(contacts, 'name', filter);
 
     return (
       <Layout>
